60b15f778f3cb5eb75582ad93a1876bfdd7acc6d
Tomasz Szuster <tomasz.szuster+github@gmail.com>
2019-04-01 21:29:42 +0200

    Initial upload - first set of library files.

---
+++ lib/airmonitor_common_libs.py
@@ -0,0 +1,69 @@
+# coding=utf-8
+
+import json
+import logging
+import os
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+API_URL = os.environ["API_URL"]
+LOG_LEVEL = os.environ["LOG_LEVEL"]
+GOOGLE_SPREADSHEET = "AirMonitor"
+KEY_FILE_PATH = "conf/gdocs_spreadsheet_auth.json"
+
+
+def logger_initialization():
+    logger = logging.getLogger(__name__)
+    formatter = logging.Formatter(f'%(name)s %(asctime)s %(levelname)s %(message)s')
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    logger.propagate = False
+    logger.setLevel(logging.DEBUG if LOG_LEVEL == "DEBUG" else logging.WARNING)
+    return logger
+
+
+# Google Docs
+def get_spreadsheet_data():
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(KEY_FILE_PATH, scope)
+    file = gspread.authorize(credentials)  # authenticate with Google
+    sheet = file.open(GOOGLE_SPREADSHEET).sheet1
+    list_from_google_docs = sheet.get_all_values()
+    return list_from_google_docs
+
+
+def _data_from_google_docs():
+    logger = logger_initialization()
+    list_from_google_docs = get_spreadsheet_data()
+
+    lat_from_google_spread = []
+    long_from_google_spread = []
+    pm_sensor_model_from_google_spread = []
+
+    logger.debug("List from Google Docs %s", list_from_google_docs[0])
+
+    for values in list_from_google_docs[1:]:
+        long = values[3]
+        lat = values[2]
+        sensor_model = values[4].upper()
+
+        lat_from_google_spread.append(lat)
+        long_from_google_spread.append(long)
+        pm_sensor_model_from_google_spread.append(sensor_model)
+
+        logger.debug("Lat %s, Long %s, Sensor model %s", lat, long, sensor_model)
+
+    return list(zip(lat_from_google_spread, long_from_google_spread, pm_sensor_model_from_google_spread))
+
+
+def _send_data_to_api(data):
+    url = API_URL
+    resp = requests.post(url, timeout=10, data=json.dumps(data), headers={"Content-Type": "application/json"})
+
+    return resp.status_code
---
+++ lib/query.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+
+from configparser import ConfigParser
+import os
+import platform
+
+from influxdb import InfluxDBClient
+
+from lib.airmonitor_common_libs import logger_initialization
+
+LOGGER = logger_initialization()
+LOG_LEVEL = os.environ["LOG_LEVEL"]
+
+if platform.system() == "Windows":
+    configuration_file_path = "C:\\configuration\\configuration.data"
+else:
+    configuration_file_path = "conf/configuration.conf"
+
+parser = ConfigParser()
+parser.read(configuration_file_path)
+
+airmonitor_database_ec2_name = (parser.get('airmonitor', 'airmonitor_database_ec2_name'))
+airmonitor_database_port = (parser.get('airmonitor', 'airmonitor_database_port'))
+airmonitor_database_username = (parser.get('airmonitor', 'airmonitor_database_username'))
+airmonitor_database_password = (parser.get('airmonitor', 'airmonitor_database_password'))
+airmonitor_database = (parser.get('airmonitor', 'airmonitor_database'))
+
+client = InfluxDBClient(
+    host=airmonitor_database_ec2_name,
+    port=airmonitor_database_port,
+    username=airmonitor_database_username,
+    password=airmonitor_database_password,
+    database=airmonitor_database,
+    ssl=True,
+    verify_ssl=False,
+    timeout=10
+)
+
+
+def query(sensor_value, lat, long):
+    if lat != ("0" or "50"):
+        influxdb_query = f"SELECT LAST(value) FROM {sensor_value} WHERE lat = '{lat}' " \
+            f"AND long = '{long}' " \
+            f"AND time > now() - 3h"
+
+        LOGGER.debug("query: %s", influxdb_query)
+        point_value = client.query(influxdb_query)
+        LOGGER.debug("point_value: %s", point_value)
+        return point_value
